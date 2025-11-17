d8c0538435b13f99b4b322985a496439c53295a0
ajitabhkumar84 <ajitabh.kumar84@gmail.com>
2025-11-17 17:19:07 +0530

    route template draft 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+<<<<<<< Updated upstream
 
+=======
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+>>>>>>> Stashed changes
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+<<<<<<< Updated upstream
+=======
+  integrations: [
+    react(),
+    sanity({
+      projectId: '2o6xnbku',
+      dataset: 'production',
+      apiVersion: '2024-01-01',
+      useCdn: true,
+      studioBasePath: '/studio',
+    }),
+  ],
+>>>>>>> Stashed changes
   vite: {
     plugins: [tailwindcss()]
   }
