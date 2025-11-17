f3e8921480a133309a86ab45c75ba651fb4e44ab
ajitabhkumar84 <ajitabh.kumar84@gmail.com>
2025-11-17 13:24:51 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { sanity } from '@sanity/astro';
+import sanity from '@sanity/astro';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
